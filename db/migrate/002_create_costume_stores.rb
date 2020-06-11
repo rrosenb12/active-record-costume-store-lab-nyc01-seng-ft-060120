4e694a228bee6f400176ac35675312bc4d61c6b3
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-06-11 15:56:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,5 +3,8 @@
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
+    create_table :costume_stores do
+      
+    end
   end
 end
