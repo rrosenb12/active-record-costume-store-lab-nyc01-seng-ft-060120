6b88e433aa2558263993207ef3e9fc2dab7b4187
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-06-11 15:53:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,5 +13,9 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
   end
 end
