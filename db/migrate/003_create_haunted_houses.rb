b74f97629567bce79ebb87c90fa6a0b4d287fb86
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-06-11 15:58:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,4 +2,6 @@
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
 class HauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+  end
 end
